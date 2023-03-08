b7464fa7a7eff8c76ea4f7384ded2fbfe8baf46c
Mratunjya Shakhdhar <mratunjya13@gmail.com>
2023-03-08 23:52:15 +0530

    Meta Data for All four Modules added

---
+++ src/meta/sign-in-up/sign-in-up-admin.js
@@ -0,0 +1,36 @@
+export const CarouselDataAdmin = {
+  desktop: [
+    {
+      heading: "Efficient Parking Management",
+      subheading:
+        "Streamline your parking operations and manage multiple parking locations from a single dashboard. It's simple and effective.",
+    },
+    {
+      heading: "Real-time Analytics",
+      subheading:
+        "Get real-time data on parking utilization, revenue, and more. Make data-driven decisions to optimize your parking business.",
+    },
+    {
+      heading: "Customizable Pricing Plans",
+      subheading:
+        "Create flexible pricing plans to fit your parking business needs. Choose from hourly, daily, weekly, or monthly rates. It's customizable and easy to use.",
+    },
+  ],
+  mobile: [
+    {
+      heading: "Efficient Management",
+      subheading:
+        "Efficiently manage parking spots and vehicles with our advanced parking system.",
+    },
+    {
+      heading: "Streamlined Operations",
+      subheading:
+        "Streamline operations with our intuitive dashboard and analytics.",
+    },
+    {
+      heading: "Real-time Analytics",
+      subheading:
+        "Get real-time analytics and insights to improve the efficiency of your parking system.",
+    },
+  ],
+};
---
+++ src/meta/sign-in-up/sign-in-up-attendant.js
@@ -0,0 +1,36 @@
+export const CarouselDataAttendant = {
+  desktop: [
+    {
+      heading: "Efficient Parking Spot Management",
+      subheading:
+        "Easily manage parking spots and assign them to customers in real-time. Keep track of parking duration and fees with just a few clicks.",
+    },
+    {
+      heading: "Real-time Customer Data",
+      subheading:
+        "Access customer information in real-time, including their parking history, payments, and contact information. Stay up-to-date on all your customers.",
+    },
+    {
+      heading: "Seamless Payment Processing",
+      subheading:
+        "Process payments quickly and easily using the app. Accept cashless payments, view transaction history, and track revenue with ease.",
+    },
+  ],
+  mobile: [
+    {
+      heading: "Easy-to-Use Interface",
+      subheading:
+        "Enjoy a user-friendly interface designed to simplify parking attendant operations.",
+    },
+    {
+      heading: "Real-time Monitoring",
+      subheading:
+        "Monitor parking spots in real-time and provide quick assistance to users.",
+    },
+    {
+      heading: "Efficient Parking Management",
+      subheading:
+        "Manage parking operations with ease and efficiency with our advanced parking system.",
+    },
+  ],
+};
