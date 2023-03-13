c3f0c6cfcaf83b8f60a43bd5503b8743b2ede55c
Mratunjya Shakhdhar <mratunjya13@gmail.com>
2023-03-13 23:35:18 +0530

    Remove Custom Scrollbar for mobile

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -9,22 +9,24 @@ const GlobalStyle = createGlobalStyle`
   * {
     outline-color: ${ACCENT_600};
 
-    ::-webkit-scrollbar {
-      width: 10px;
-      height: 10px;
-    }
+    @media (min-width: 769px) {
+      ::-webkit-scrollbar {
+        width: 10px;
+        height: 10px;
+      }
 
-    ::-webkit-scrollbar-track {
-      background: transparent;
-    }
+      ::-webkit-scrollbar-track {
+        background: transparent;
+      }
 
-    ::-webkit-scrollbar-thumb {
-      background: ${ACCENT_600};
-      border-radius: 5px;
-    }
+      ::-webkit-scrollbar-thumb {
+        background: ${ACCENT_600};
+        border-radius: 5px;
+      }
 
-    ::-webkit-scrollbar-thumb:hover {
-      background: ${ACCENT_700};
+      ::-webkit-scrollbar-thumb:hover {
+        background: ${ACCENT_700};
+      }
     }
   }
 `;
