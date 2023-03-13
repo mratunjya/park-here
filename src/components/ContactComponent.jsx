bf9653b8b82e07350fd6ca736ce9bed91c960908
Mratunjya Shakhdhar <mratunjya13@gmail.com>
2023-03-13 16:12:58 +0530

    Contact Page Added

---
+++ src/components/ContactComponent.jsx
@@ -0,0 +1,114 @@
+// Common components
+import CommonImage from "@components/common/CommonImage";
+import { H1, H4, P } from "@components/common/Headings";
+import CommonHead from "@components/common/CommonHead";
+import FlexBox from "@components/common/FlexBox";
+
+// Data
+import CommonLayout from "@layout/common/CommonLayout";
+import { PRIMARY_400, WHITE } from "@constants/colors";
+import { ContactData } from "@meta/contact";
+
+const ContactComponent = () => {
+  return (
+    <>
+      <CommonHead title="Park Here: About" />
+      <CommonLayout
+        backgroundcolor={PRIMARY_400}
+        backgroundcolormobile="transparent"
+      >
+        <FlexBox
+          width="86.67%"
+          maxwidth="75rem"
+          margin="0 auto"
+          padding="2.5rem"
+          paddingmobile="1.5rem 0"
+          gap="3rem"
+          gapmobile="2rem"
+          wrapmobile="wrap-reverse"
+          align="flex-start"
+          justify="center"
+          backgroundcolor={WHITE}
+          minheight="100%"
+          overflow="auto"
+        >
+          <FlexBox
+            width="50%"
+            widthmobile="100%"
+            direction="column"
+            gap="1.5rem"
+          >
+            <H1 bold>{ContactData.heading}</H1>
+            <P>{ContactData.paragraphs[1]}</P>
+            <FlexBox
+              direction="column"
+              width="100%"
+              gap="0.75rem"
+              height="fit-content"
+            >
+              <H4 bold>{ContactData.subHeadings[1]}</H4>
+              <P>{ContactData.contactInfo.p}</P>
+              <ul
+                style={{
+                  paddingLeft: "1.25rem",
+                }}
+              >
+                <li>
+                  <P>
+                    <strong>Email:</strong> {ContactData.contactInfo.email}
+                  </P>
+                </li>
+                <li>
+                  <P>
+                    <strong>Phone:</strong> {ContactData.contactInfo.phone}
+                  </P>
+                </li>
+                <li>
+                  <P>
+                    <strong>Address:</strong> {ContactData.contactInfo.address}
+                  </P>
+                </li>
+              </ul>
+            </FlexBox>
+            <FlexBox
+              direction="column"
+              width="100%"
+              gap="0.75rem"
+              height="fit-content"
+            >
+              <H4 bold>{ContactData.subHeadings[2]}</H4>
+              <P>{ContactData.paragraphs[2]}</P>
+            </FlexBox>
+            <FlexBox
+              direction="column"
+              width="100%"
+              gap="0.75rem"
+              height="fit-content"
+            >
+              <H4 bold>{ContactData.subHeadings[3]}</H4>
+              <P>{ContactData.paragraphs[3]}</P>
+            </FlexBox>
+            <H4 bold>{ContactData.contactForm.heading}</H4>
+          </FlexBox>
+          <CommonImage
+            src={ContactData.image}
+            width="50%"
+            widthmobile="100%"
+            alt="About Visual"
+            objectposition="top center"
+            height="fit-content"
+            quality={1}
+            priority={true}
+            position="sticky"
+            positionmobile="unset"
+            top="0"
+            left="0"
+            alignself="flex-start"
+          />
+        </FlexBox>
+      </CommonLayout>
+    </>
+  );
+};
+
+export default ContactComponent;
---
+++ src/meta/contact.js
@@ -0,0 +1,26 @@
+import ContactSvg from "@assets/contact/contact.svg";
+
+export const ContactData = {
+  heading: "Contact Us",
+  subHeadings: {
+    1: "Contact Information",
+    2: "Working Hours",
+    3: "Send Us a Message",
+  },
+  paragraphs: {
+    1: "We are always happy to hear from you! Whether you have questions, suggestions or feedback, please feel free to reach out to us.",
+    2: "Our working hours are from [insert opening time] to [insert closing time], Monday to Friday. We are closed on weekends and public holidays.",
+    3: "You can also send us a message through the contact form below. Our team will get back to you as soon as possible.",
+  },
+  contactInfo: {
+    p: "You can contact us through any of the following channels:",
+    email: "insert email address",
+    phone: "insert phone number",
+    address: "insert physical address",
+  },
+  contactForm: {
+    heading:
+      "Thank you for reaching out to us! We look forward to hearing from you soon.",
+  },
+  image: ContactSvg,
+};
