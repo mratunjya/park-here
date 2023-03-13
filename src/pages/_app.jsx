a7ac7c4ccd56f51bdd2e7cf6c50491b89774dec1
Mratunjya Shakhdhar <mratunjya13@gmail.com>
2023-03-13 23:31:19 +0530

    Custom Scroll Bar Added

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,6 @@
 import NextNProgress from "nextjs-progressbar";
 import CommonHead from "@common/CommonHead";
-import { PRIMARY_900, ACCENT_600 } from "@constants/colors";
+import { PRIMARY_900, ACCENT_700, ACCENT_600 } from "@constants/colors";
 import { createGlobalStyle } from "styled-components";
 import "../styles/globals.css";
 import CommonNavBar from "@components/common/NavBar";
@@ -8,6 +8,24 @@ import CommonNavBar from "@components/common/NavBar";
 const GlobalStyle = createGlobalStyle`
   * {
     outline-color: ${ACCENT_600};
+
+    ::-webkit-scrollbar {
+      width: 10px;
+      height: 10px;
+    }
+
+    ::-webkit-scrollbar-track {
+      background: transparent;
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background: ${ACCENT_600};
+      border-radius: 5px;
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+      background: ${ACCENT_700};
+    }
   }
 `;
 
