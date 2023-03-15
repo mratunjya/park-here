bd847e329223b7e391aa96dbff1eb08e5b62e1bf
Mratunjya Shakhdhar <mratunjya13@gmail.com>
2023-03-15 12:18:59 +0530

    Added Title for 404 page

--- src/components/common/404.jsx
+++ src/components/common/404.jsx
@@ -1,47 +1,48 @@
+import CommonHead from "@components/common/CommonHead";
+import CommonLayout from "@layout/common/CommonLayout";
+import { ACCENT_900, WHITE } from "@constants/colors";
 import FlexBox from "@components/common/FlexBox";
 import { H1 } from "@components/common/Headings";
-import { ACCENT_900, WHITE } from "@constants/colors";
-import CommonLayout from "@layout/common/CommonLayout";
-import styled from "styled-components";
-
-export const PageNotFoundWrapper = styled(FlexBox)``;
 
 const PageNotFound = () => {
   return (
-    <CommonLayout>
-      <PageNotFoundWrapper
-        width="100%"
-        backgroundcolor={ACCENT_900}
-        align="center"
-        justify="center"
-        height="100%"
-      >
+    <>
+      <CommonHead title="Park Here: Error 404, Page Not Found" />
+      <CommonLayout>
         <FlexBox
-          width="fit-content"
-          gap="0.125rem"
-          backgroundcolor={WHITE}
+          width="100%"
+          backgroundcolor={ACCENT_900}
           align="center"
           justify="center"
+          height="100%"
         >
-          <H1
-            color={WHITE}
-            bold
-            backgroundcolor={ACCENT_900}
-            padding="0.5rem 1rem"
+          <FlexBox
+            width="fit-content"
+            gap="0.125rem"
+            backgroundcolor={WHITE}
+            align="center"
+            justify="center"
           >
-            404
-          </H1>
-          <H1
-            color={WHITE}
-            bold
-            backgroundcolor={ACCENT_900}
-            padding="0.5rem 1rem"
-          >
-            Page Not Found
-          </H1>
+            <H1
+              color={WHITE}
+              bold
+              backgroundcolor={ACCENT_900}
+              padding="0.5rem 1rem"
+            >
+              404
+            </H1>
+            <H1
+              color={WHITE}
+              bold
+              backgroundcolor={ACCENT_900}
+              padding="0.5rem 1rem"
+            >
+              Page Not Found
+            </H1>
+          </FlexBox>
         </FlexBox>
-      </PageNotFoundWrapper>
-    </CommonLayout>
+      </CommonLayout>
+    </>
   );
 };
 
