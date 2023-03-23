8a0f145a70ec481ce62d98fab947f0aab3afe6e1
Mratunjya Shakhdhar <mratunjya13@gmail.com>
2023-03-23 13:35:34 +0530

    dashboard Options added

---
+++ src/meta/DashBoardOptions/options.js
@@ -0,0 +1,16 @@
+export const options = {
+  admin: ["Manage Parking Lots", "Parking Lot Status", "Manage Users"],
+  user: ["Parking Spots", "Parking History"],
+  organization: [
+    "Manage Parking Lots",
+    "Manage Attendants",
+    "Parking Lot Status",
+    "Generate Reports",
+  ],
+  attendant: [
+    "Monitor Parking Lot",
+    "Issue Ticket",
+    "Collect Fee",
+    "View Parking History",
+  ],
+};
