e1220582809dfbf733c415c95a23b517a6c03a2d
Mratunjya Shakhdhar <mratunjya13@gmail.com>
2023-03-23 15:42:15 +0530

    DashBoard page Added

---
+++ src/pages/dashboard/[dashboard-options].js
@@ -0,0 +1,71 @@
+import DashBoardOptionsRight from "@components/dashboard/DashBoardOptionsRight";
+import DashboardOptionsLeft from "@components/dashboard/DashBoardOptionsLeft";
+import { options } from "@meta/DashBoardOptions/options";
+import PageNotFound from "@components/common/404";
+import FlexBox from "@components/common/FlexBox";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import styled from "styled-components";
+import localforage from "localforage";
+
+const DashboardOptionstWrapper = styled(FlexBox)`
+  & > *:first-child {
+    flex: 1;
+  }
+
+  & > *:last-child {
+    flex: 4;
+  }
+`;
+
+const Dashboard = () => {
+  const [possibleRoutes, setPossibleRoutes] = useState(null);
+  const [module, setModule] = useState(null);
+  const [height, setHeight] = useState(0);
+  const [data, setData] = useState(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    setHeight(localStorage.getItem("navBarHeight"));
+    let allowedRoutes = [];
+    let allMenuOptions = [];
+    localforage.getItem("module").then((module) => {
+      setModule(module);
+      allMenuOptions = options[`${module}`]?.map((option) =>
+        option
+          .toLocaleLowerCase()
+          .replaceAll("&", "")
+          .split(" ")
+          .filter((item) => item !== "")
+          .join("-")
+      );
+      allowedRoutes.push(module);
+      allowedRoutes.push("profile");
+      allowedRoutes.push(...allMenuOptions);
+      setPossibleRoutes([...allowedRoutes]);
+    });
+  }, [router]);
+
+  useEffect(() => {
+    localforage.getItem("data").then((d) => {
+      setData(d);
+    });
+  }, []);
+
+  return (
+    possibleRoutes !== null &&
+    (possibleRoutes?.includes(router?.query["dashboard-options"]) ? (
+      <DashboardOptionstWrapper
+        width="100%"
+        height={`calc(100vh - ${height}px)`}
+      >
+        <DashboardOptionsLeft module={module} />
+        <DashBoardOptionsRight module={module} data={data} />
+      </DashboardOptionstWrapper>
+    ) : (
+      <PageNotFound />
+    ))
+  );
+};
+
+export default Dashboard;
